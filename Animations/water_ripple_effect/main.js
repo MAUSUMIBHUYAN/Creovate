a1a8681390227df508a120055224d4b55d288cbf
Mausumi Bhuyan <122201001@smail.iitpkd.ac.in>
2025-03-30 06:00:48 +0530

    back to previous

--- Animations/water_ripple_effect/main.js
+++ Animations/water_ripple_effect/main.js
@@ -1,17 +1,17 @@
-$(document).ready(() => {
+$(document).ready(()=>{
 	$(".hero").ripples({
 		resolution: 512,
 		dropRadius: 30,
 		perturbance: 0.01,
 	});
 
-	setInterval(() => {
-		let $el = $(".hero");
-		let x = Math.random() * $el.outerWidth();
-		let y = Math.random() * $el.outerHeight();
-		let dropRadius = 50;
-		let strength = 0.04 + Math.random() * 0.04;
+	setInterval(function () {
+		var $el = $(".hero");
+		var x = Math.random() * $el.outerWidth();
+		var y = Math.random() * $el.outerHeight();
+		var dropRadius = 50;
+		var strength = 0.04 + Math.random() * 0.04;
 
-		$el.ripples("drop", x, y, dropRadius, strength);
+		$el.ripples("drop",x,y,dropRadius,strength);
 	}, 2000);
 });
